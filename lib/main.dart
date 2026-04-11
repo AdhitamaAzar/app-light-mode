d67502d73b1aa62bfeee18174b92188dbd25da11
AdhitamaAzar <adhitamagaming969@gmail.com>
2026-04-11 14:09:04 +0700

    Modul 18

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:light_mode/auth/auth_gate.dart%20%20';
 import 'package:light_mode/themes/light_mode.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:light_mode/themes/theme_provider.dart';
+import 'package:provider/provider.dart';
 import 'package:light_mode/pages/home_page.dart';
 import 'firebase_options.dart';
 
@@ -9,7 +11,13 @@ void main() async {
 WidgetsFlutterBinding.ensureInitialized(); await
 Firebase.initializeApp( options:
 DefaultFirebaseOptions.currentPlatform, );
-runApp(const MyApp());}
+      runApp(
+      ChangeNotifierProvider(
+        create: (context) => ThemeProvider(),
+        child: const MyApp(),
+      ),
+    );
+  }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -19,7 +27,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: const AuthGate(),
-      theme: lightmode,
+      theme: Provider.of<ThemeProvider>(context).themeData ,
 
     routes: {
     '/home': (context) => HomePage(), // 🔥 WAJIB
---
+++ lib/themes/theme_provider.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'dark_mode.dart';
+import 'package:light_mode/themes/light_mode.dart';
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeData _themeData = lightmode;
+
+  ThemeData get themeData => _themeData;
+
+  bool get isDarkMode => _themeData == darkMode;
+
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+    notifyListeners();
+  }
+
+  void toggleTheme() {
+    if (_themeData == lightmode) {
+      themeData = darkMode;
+    } else {
+      themeData = lightmode;
+    }
+    notifyListeners();
+  }
+}
